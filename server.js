234b7ab9a1be8dd959aecdbade71e44d65ef784e
Morry Kolman <morristskolman@gmail.com>
2023-09-18 14:07:43 -0400

    api fiddling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 6969
 const fetch = require('node-fetch')
 require('dotenv').config()
 const path = require('path');
@@ -15,8 +15,30 @@ app.get('/', (req, res) => {
   
 
 //don't overcall API. Update max every 5 minutes.
-let lastCalled = 0
+let lastCalledUpdateVideo = 0
+let lastCalledUpdateList = 0
 let currentVideoID = ''
+// let currentVideoIDList = []
+let currentVideoIDList = [
+  'qKwSyQ0E2MU', 'VEDhPveofv0', 'wJQ-c8puy64',
+  '-oYYkv4-_3E', 'LiGUDtcMQhE', 'qqcKUh5QJv0',
+  'URIcltCDrnk', 'Z5pX_6OlgFA', 'OojFdgn6WFU',
+  'Z6SV1rX9rZQ', 'SM7PS2LfXYY', 'Ahal2BCaofo',
+  '2Bf2UW6guwk', 'oNqMRO4N7fM', 'fo-TVP3IIvY',
+  'Z2LwSfwVObA', 'hUaiybWYzhs', 'EOoNTYlw5lk',
+  'wsOnd22eQS4', 'ONOa5jaiGR8', 'iIJ1kMYQbbg',
+  'XqzhA0Xv3Ug', 'CnUGA8OVAWU', '_OT59eGen7A',
+  'AV8n8MhFHX8', 'JnwB3l0_5rg', 'Q_9dHtCYoOo',
+  'yDONpKhXjq0', 'raSjvhOFsZc', 'cLtRshsDCl8',
+  'QPxgcsWPbFw', 'LCC-AQS11-4', 'BnF6dezLTBw',
+  '6njM1A9MMjM', 'cTLzRIdP688', 'OHvWMJWVtRI',
+  'wfXI1xjyQ6Q', 'ubXCW6d1LNs', 'By2C5uCzqjA',
+  '8TbunazUmTE', 'gdBLLwFaWRk', 'L8x7UBhJAiI',
+  'sbaIwjZNiss', 'uFtYS41dMZM', '2cIjoxu6W7k',
+  'e-Sps-Edo7A', 'Rgb_TrNMoOQ', 'rm4BvHCjijk',
+  'SSuc5S0Rxyk', '9rcDaAhPfyU'
+]
+let currentVideoIDObjects = []
 
 
 //get relevant youtube data
@@ -24,13 +46,16 @@ const updateVideoID = async () => {
     //get current time
     let currentTime = Date.now();
     //if current time is within 5 minutes of last call, return last video id
-    if (currentTime - lastCalled < (1000 * 60 *5)) return currentVideoID
+    if (currentTime - lastCalledUpdateVideo < (1000 * 60 *5)) return currentVideoID
     //else, get the new info from fetch
-    const newVideoID = await fetch(`https://youtube.googleapis.com/youtube/v3/search?order=date&q=test%20ignore&type=video&videoEmbeddable=any&key=${YOUTUBE_API_KEY}`)
+    const newVideoID = await fetch(`https://youtube.googleapis.com/youtube/v3/search?order=date&q=test%20ignore&type=video&maxResults=50&videoEmbeddable=any&key=${YOUTUBE_API_KEY}`)
         .then(res => res.json())
         .then(data => {
             console.log('in fetch')
+            // console.log(data.items)
             currentVideoID = data.items[0].id.videoId
+            currentVideoIDList = data.items.map(el => el.id.videoId)
+            console.log(currentVideoIDList)
             return currentVideoID})
         .catch(error => {
             console.log('ERROR in getData fetch:', error)
@@ -45,6 +70,34 @@ const makeIFrame = () => {
     src="https://www.youtube.com/embed/${currentVideoID}">
     </iframe> <br/> <p><i>Not on our watch. </br>-WMI</i></p>`
 }
+
+const getReverseSortedVideoViewList = async () => {
+    let currentTime = Date.now();
+    //if current time is within 5 minutes of last call, return last video id
+    if (currentTime - lastCalledUpdateList < (1000 * 60 *5)) return currentVideoList
+    //else, get the new info from fetch
+    let idString = ''
+    currentVideoIDList.forEach(el => {
+        idString += `${el}%2C`
+    })
+    idString = idString.slice(0,-3)
+    console.log(idString)
+    const newVideoID = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=${idString}&key=${YOUTUBE_API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log('in reverse seated fetch')
+            console.log(data)
+            // currentVideoID = data.items[0].id.videoId
+            return currentVideoID})
+        .catch(error => {
+            console.log('ERROR in getData fetch:', error)
+            return currentVideoID
+        })
+    console.log(newVideoID)
+    return newVideoID
+}
+
+getReverseSortedVideoViewList()
 
 app.get('/recentVid', async (req, res) => {
     await updateVideoID()
