8d2158bde164888053c448e67652609b861f3e30
Alexander Petros <apetros15@gmail.com>
2023-10-15 14:49:59 -0400

    Remove reverse sorted list

--- server.js
+++ server.js
@@ -11,22 +11,14 @@ const YOUTUBE_SEARCH_URL = `https://youtube.googleapis.com/youtube/v3/search?ord
 let lastCalledUpdateVideo = 0
 let lastCalledUpdateList = 0
 let currentVideoIDList = [
-  'qKwSyQ0E2MU', 'VEDhPveofv0', 'wJQ-c8puy64',
-  '-oYYkv4-_3E', 'LiGUDtcMQhE', 'qqcKUh5QJv0',
-  'URIcltCDrnk', 'Z5pX_6OlgFA', 'OojFdgn6WFU',
-  'Z6SV1rX9rZQ', 'SM7PS2LfXYY', 'Ahal2BCaofo',
-  '2Bf2UW6guwk', 'oNqMRO4N7fM', 'fo-TVP3IIvY',
-  'Z2LwSfwVObA', 'hUaiybWYzhs', 'EOoNTYlw5lk',
-  'wsOnd22eQS4', 'ONOa5jaiGR8', 'iIJ1kMYQbbg',
-  'XqzhA0Xv3Ug', 'CnUGA8OVAWU', '_OT59eGen7A',
-  'AV8n8MhFHX8', 'JnwB3l0_5rg', 'Q_9dHtCYoOo',
-  'yDONpKhXjq0', 'raSjvhOFsZc', 'cLtRshsDCl8',
-  'QPxgcsWPbFw', 'LCC-AQS11-4', 'BnF6dezLTBw',
-  '6njM1A9MMjM', 'cTLzRIdP688', 'OHvWMJWVtRI',
-  'wfXI1xjyQ6Q', 'ubXCW6d1LNs', 'By2C5uCzqjA',
-  '8TbunazUmTE', 'gdBLLwFaWRk', 'L8x7UBhJAiI',
-  'sbaIwjZNiss', 'uFtYS41dMZM', '2cIjoxu6W7k',
-  'e-Sps-Edo7A', 'Rgb_TrNMoOQ', 'rm4BvHCjijk',
+  'qKwSyQ0E2MU', 'VEDhPveofv0', 'wJQ-c8puy64', '-oYYkv4-_3E', 'LiGUDtcMQhE', 'qqcKUh5QJv0',
+  'URIcltCDrnk', 'Z5pX_6OlgFA', 'OojFdgn6WFU', 'Z6SV1rX9rZQ', 'SM7PS2LfXYY', 'Ahal2BCaofo',
+  '2Bf2UW6guwk', 'oNqMRO4N7fM', 'fo-TVP3IIvY', 'Z2LwSfwVObA', 'hUaiybWYzhs', 'EOoNTYlw5lk',
+  'wsOnd22eQS4', 'ONOa5jaiGR8', 'iIJ1kMYQbbg', 'XqzhA0Xv3Ug', 'CnUGA8OVAWU', '_OT59eGen7A',
+  'AV8n8MhFHX8', 'JnwB3l0_5rg', 'Q_9dHtCYoOo', 'yDONpKhXjq0', 'raSjvhOFsZc', 'cLtRshsDCl8',
+  'QPxgcsWPbFw', 'LCC-AQS11-4', 'BnF6dezLTBw', '6njM1A9MMjM', 'cTLzRIdP688', 'OHvWMJWVtRI',
+  'wfXI1xjyQ6Q', 'ubXCW6d1LNs', 'By2C5uCzqjA', '8TbunazUmTE', 'gdBLLwFaWRk', 'L8x7UBhJAiI',
+  'sbaIwjZNiss', 'uFtYS41dMZM', '2cIjoxu6W7k', 'e-Sps-Edo7A', 'Rgb_TrNMoOQ', 'rm4BvHCjijk',
   'SSuc5S0Rxyk', '9rcDaAhPfyU'
 ]
 
@@ -73,26 +65,4 @@ function getVideoFrame (index = 0) {
             hx-swap-oob=true>
     </iframe>
   `
-}
-
-async function getReverseSortedVideoViewList () {
-  let currentTime = Date.now()
-  //if current time is within 5 minutes of last call, return last video id
-  if (currentTime - lastCalledUpdateList < (1000 * 60 *5)) return currentVideoList
-  //else, get the new info from fetch
-  let idString = ''
-  currentVideoIDList.forEach(el => {
-    idString += `${el}%2C`
-  })
-  idString = idString.slice(0,-3)
-  const newVideoID = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=${idString}&key=${YOUTUBE_API_KEY}`)
-    .then(res => res.json())
-    .then(data => {
-      // currentVideoID = data.items[0].id.videoId
-      return currentVideoID})
-    .catch(error => {
-      console.log('ERROR in getData fetch:', error)
-      return currentVideoID
-    })
-  return newVideoID
 }
