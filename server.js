94ce06311ec9b89957fe6d1563ea1d76242e3b50
Alexander Petros <apetros15@gmail.com>
2023-10-15 13:39:14 -0400

    Switch to function syntax

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ require('dotenv').config()
 const path = require('path');
 
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
+const YOUTUBE_URL = `https://youtube.googleapis.com/youtube/v3/search?order=date&q=test%20ignore&type=video&maxResults=50&videoEmbeddable=any&key=${YOUTUBE_API_KEY}`
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
-
-
 
 //don't overcall API. Update max every 5 minutes.
 let lastCalledUpdateVideo = 0
@@ -40,13 +39,13 @@ let currentVideoIDObjects = []
 
 
 //get relevant youtube data
-const updateVideoID = async () => {
+async function updateVideoID () {
     //get current time
     let currentTime = Date.now();
     //if current time is within 5 minutes of last call, return last video id
     if (currentTime - lastCalledUpdateVideo < (1000 * 60 *5)) return currentVideoID
     //else, get the new info from fetch
-    const newVideoID = await fetch(`https://youtube.googleapis.com/youtube/v3/search?order=date&q=test%20ignore&type=video&maxResults=50&videoEmbeddable=any&key=${YOUTUBE_API_KEY}`)
+    const newVideoID = await fetch(YOUTUBE_URL)
         .then(res => res.json())
         .then(data => {
             currentVideoID = data.items[0].id.videoId
@@ -58,13 +57,13 @@ const updateVideoID = async () => {
     return newVideoID
 }
 
-const makeIFrame = () => {
+async function makeIFrame () {
     return `<iframe style="width: 100%; height : 100%; display:block; margin: auto"
     src="https://www.youtube.com/embed/${currentVideoID}">
     </iframe> <br/> <p><i>Not on our watch. </br>-WMI</i></p>`
 }
 
-const getReverseSortedVideoViewList = async () => {
+async function getReverseSortedVideoViewList () {
     let currentTime = Date.now();
     //if current time is within 5 minutes of last call, return last video id
     if (currentTime - lastCalledUpdateList < (1000 * 60 *5)) return currentVideoList
