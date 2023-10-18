2f979b42211b71106a1d6f66813ed7d134f8f484
Alexander Petros <apetros15@gmail.com>
2023-10-18 17:29:13 -0400

    Package for fly deployment

--- server.js
+++ server.js
@@ -8,27 +8,16 @@ const PORT = 8080
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
 const YOUTUBE_SEARCH_URL = `https://youtube.googleapis.com/youtube/v3/search?order=date&q=test%20ignore&type=video&maxResults=50&videoEmbeddable=any&key=${YOUTUBE_API_KEY}`
 
-//don't overcall API. Update max every 5 minutes.
 let lastCalledUpdateVideo = 0
-let lastCalledUpdateList = 0
-let currentVideoIDList = [
-  'qKwSyQ0E2MU', 'VEDhPveofv0', 'wJQ-c8puy64', '-oYYkv4-_3E', 'LiGUDtcMQhE', 'qqcKUh5QJv0',
-  'URIcltCDrnk', 'Z5pX_6OlgFA', 'OojFdgn6WFU', 'Z6SV1rX9rZQ', 'SM7PS2LfXYY', 'Ahal2BCaofo',
-  '2Bf2UW6guwk', 'oNqMRO4N7fM', 'fo-TVP3IIvY', 'Z2LwSfwVObA', 'hUaiybWYzhs', 'EOoNTYlw5lk',
-  'wsOnd22eQS4', 'ONOa5jaiGR8', 'iIJ1kMYQbbg', 'XqzhA0Xv3Ug', 'CnUGA8OVAWU', '_OT59eGen7A',
-  'AV8n8MhFHX8', 'JnwB3l0_5rg', 'Q_9dHtCYoOo', 'yDONpKhXjq0', 'raSjvhOFsZc', 'cLtRshsDCl8',
-  'QPxgcsWPbFw', 'LCC-AQS11-4', 'BnF6dezLTBw', '6njM1A9MMjM', 'cTLzRIdP688', 'OHvWMJWVtRI',
-  'wfXI1xjyQ6Q', 'ubXCW6d1LNs', 'By2C5uCzqjA', '8TbunazUmTE', 'gdBLLwFaWRk', 'L8x7UBhJAiI',
-  'sbaIwjZNiss', 'uFtYS41dMZM', '2cIjoxu6W7k', 'e-Sps-Edo7A', 'Rgb_TrNMoOQ', 'rm4BvHCjijk',
-  'SSuc5S0Rxyk', '9rcDaAhPfyU'
-]
+let currentVideoIDList = []
 
 const app = express()
 nunjucks.configure('.', { express: app })
+await refreshSearch()
 
 app.use('/static', express.static('static'))
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   const iframe = getVideoFrame(1)
   res.render('./index.html', { iframe })
 })
@@ -54,9 +43,6 @@ app.listen(PORT, () => {
 })
 
 async function refreshSearch () {
-  //if current time is within 5 minutes of last call, don't refres
-  if (currentTime - lastCalledUpdateVideo < (1000 * 60 *5)) return
-  //else, get the new info from fetch
   try {
     const apiRes = await fetch(YOUTUBE_SEARCH_URL)
     const data = await apiRes.json()
